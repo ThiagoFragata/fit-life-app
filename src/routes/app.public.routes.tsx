310a1c7545df0960a56b4cd165bae589c6f91076
Thiago Henrique Fragata <thiago.henrique.fragata@hotmail.com>
2024-05-28 10:43:37 -0400

    refactor: melhoria verificação da onboading

--- src/routes/app.public.routes.tsx
+++ src/routes/app.public.routes.tsx
@@ -14,6 +14,7 @@ const Stack = createNativeStackNavigator();
 
 export function PublicRoutes() {
   const { isViewOnboarding } = useOnboardingStore();
+  console.log(isViewOnboarding);
 
   return (
     <Stack.Navigator
--- src/screens/public/onboarding/stores/useOnboarding.ts
+++ src/screens/public/onboarding/stores/useOnboarding.ts
@@ -1,23 +1,38 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
 
-interface useOnboardingStoreProps {
+interface OnboardingState {
   isViewOnboarding: boolean;
   onViewOnboarding: () => void;
 }
 
-export const useOnboardingStore = create<useOnboardingStoreProps>()((set) => ({
+export const useOnboardingStore = create<OnboardingState>()((set) => ({
   isViewOnboarding: false,
   onViewOnboarding: () => set({ isViewOnboarding: true }),
 }));
 
-AsyncStorage.getItem('onboardingState').then((data) => {
-  if (data) {
-    const persistedState = JSON.parse(data);
-    useOnboardingStore.setState(persistedState);
+async function loadOnboardingState() {
+  try {
+    const storedState = await AsyncStorage.getItem('onboardingState');
+    if (storedState) {
+      const parsedState: OnboardingState = JSON.parse(storedState);
+      if (typeof parsedState.isViewOnboarding === 'boolean') {
+        useOnboardingStore.setState(parsedState);
+      } else {
+        console.warn('Invalid onboarding state in storage');
+      }
+    }
+  } catch (error) {
+    console.error('Error loading onboarding state:', error);
+  }
+}
+
+useOnboardingStore.subscribe(async (state) => {
+  try {
+    await AsyncStorage.setItem('onboardingState', JSON.stringify(state));
+  } catch (error) {
+    console.error('Error saving onboarding state:', error);
   }
 });
 
-useOnboardingStore.subscribe((state) => {
-  AsyncStorage.setItem('onboardingState', JSON.stringify(state));
-});
+loadOnboardingState();
